bc297cd1a6144c8a0126b96abd6f23c985f6260e
Nathan Stoltenberg <ataranlen@gmail.com>
2016-01-08 18:36:44 -0600

    Battledome Spawn Task base

---
+++ civcraft/src/com/avrgaming/civcraft/threading/tasks/BattledomeMobSpawnTask.java
@@ -0,0 +1,58 @@
+package com.avrgaming.civcraft.threading.tasks;
+
+import java.util.HashSet;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+import com.avrgaming.civcraft.main.CivLog;
+import com.avrgaming.civcraft.structure.wonders.Battledome;
+import com.avrgaming.civcraft.structure.wonders.Wonder;
+import com.avrgaming.civcraft.threading.CivAsyncTask;
+
+public class BattledomeMobSpawnTask extends CivAsyncTask {
+
+	Battledome battledome;
+	
+	public static HashSet<String> debugTowns = new HashSet<String>();
+
+	public static void debug(Battledome battledome, String msg) {
+		if (debugTowns.contains(battledome.getTown().getName())) {
+			CivLog.warning("BattledomeDebug:"+battledome.getTown().getName()+":"+msg);
+		}
+	}	
+	
+	public BattledomeMobSpawnTask(Wonder battledome) {
+		this.battledome = (Battledome)battledome;
+	}
+	
+	public void processBattledomeSpawn() {
+		if (!battledome.isActive()) {
+			debug(battledome, "Battledome inactive...");
+			return;
+		}
+		World world = Bukkit.getWorld("world");
+
+        world.spawnCreature(pLoc, EntityType.SKELETON);
+		
+		debug(battledome, "Processing Battledome...");
+	}
+	
+	@Override
+	public void run() {
+		if (this.battledome.lock.tryLock()) {
+			try {
+				try {
+					processBattledomeSpawn();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} finally {
+				this.battledome.lock.unlock();
+			}
+		} else {
+			debug(this.battledome, "Failed to get lock while trying to start task, aborting.");
+		}
+	}
+
+}
